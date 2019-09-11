eaa5bc68f69293d1f13d1ab46dc8f371c95ce0ab
Sean Lee <desperli@gmail.com>
2019-09-11 14:08:23 +0800

    update

--- system_status_snapshot.sh
+++ system_status_snapshot.sh
@@ -2,7 +2,9 @@
 FOLDER=$(hostname)_$(date +"%F_%T")
 mkdir -p $FOLDER
 
-free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }' > $FOLDER/overview.txt
+hostname > $FOLDER/overview.txt
+uname -a >> $FOLDER/overview.txt
+free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }' >> $FOLDER/overview.txt
 df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}' >> $FOLDER/overview.txt
 # top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}' >> $FOLDER/overview.txt
 
